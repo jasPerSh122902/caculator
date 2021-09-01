2696700e1b4524b6c46065a30d5fb433d79c7f36
jasPerSh122902 <88592760+jasPerSh122902@users.noreply.github.com>
2021-09-01 14:26:11 -0500

    added the options and cleaned the it up.
    still error in the getting in the loop part and the showing the caculations.

--- caculator/caculator/caculator/Game.cs
+++ caculator/caculator/caculator/Game.cs
@@ -6,96 +6,107 @@ namespace caculator
 
     class Game
     {
+        //the numbers that are needed
         public int number1 = 0;
         public int number2 = 0;
+        //the result value
         public int result = 0;
+
+        //the input 
         string input = "";
-       public float inputvalue = 0;
+        public float inputvalue = 0;
         public string inputRecieved = Console.ReadLine();
+
+        //the functions
         public int Add(int number1, int number2)
         {
 
             //the adding process
+
             Console.ReadLine();
-            return number1 += number2;
+            return result = number1 += number2;
 
         }
 
         public float multiply(int number1, int number2)
         {
+
+            //multiply
             Console.ReadLine();
-            return number1 *= number2;
+            return result = number1 *= number2;
         }
 
         public float divide(int number1, int number2)
         {
+
+            //dividing process 
             Console.ReadLine();
             return result = number1 %= number2;
         }
 
         public int subtract(int number1, int number2)
         {
+            //subtraction 
             Console.ReadLine();
             return result = number1 -= number2;
         }
         public void Run()
         {
+            //the beging lines that come in 
             Console.WriteLine("caculations");
             Console.WriteLine("write +, -, *, %_Write the word");
-            Console.ReadLine();
-            if(!float.TryParse(input, out inputvalue))
-            {
-                if (inputRecieved == "plus" || inputRecieved == "subtract" || inputRecieved == "multipy" || inputRecieved == "divide")
+
+            //if the input is some word then that word is going to spit out a number from the if statment
+
+                Console.ReadLine();
+                if (inputRecieved == "plus" || inputRecieved == "+")
                 {
-                    if (inputRecieved == "plus" || inputRecieved == "+")
-                    {
 
-                        Add(0, 0);
-                        Console.ReadLine();
-                        Console.WriteLine("product = ", result);
-                        Run();
-                    }
+                    Add(number1, number2);
+                    Console.ReadLine();
+                    Console.WriteLine("product = ", number1);
+                    Console.Clear();
+                    Run();
+                }
 
-                    else if (inputRecieved == "subtract" || inputRecieved == "-")
-                    {
+                else if (inputRecieved == "subtract" || inputRecieved == "-")
+                {
 
-                        subtract(0, 0);
-                        Console.ReadLine();
-                        Console.WriteLine();
-                        Run();
-                    }
+                    subtract(number1, number2);
+                    Console.ReadLine();
+                    Console.WriteLine("product = ", result);
+                    Console.Clear();
+                    Run();
+                }
 
-                    else if (inputRecieved == "multiply" || inputRecieved == "*")
-                    {
+                else if (inputRecieved == "multiply" || inputRecieved == "*")
+                {
 
-                        multiply(0, 0);
-                        Console.ReadLine();
-                        Console.WriteLine();
-                        Run();
-                    }
+                    multiply(number1, number2);
+                    Console.ReadLine();
+                    Console.WriteLine("product = ", result);
+                    Console.Clear();
+                    Run();
+                }
 
-                    else if (inputRecieved == "divide" || inputRecieved == "%")
-                    {
+                else if (inputRecieved == "divide" || inputRecieved == "%")
+                {
 
-                        divide(0, 0);
-                        Console.ReadLine();
-                        Console.WriteLine();
-                        Run();
-                    }
+                    divide(number1, number2);
+                    Console.ReadLine();
+                    Console.WriteLine("product = ", result);
+                    Console.Clear();
+                    Run();
+                }
 
-                    else
-                    {
-                        Console.ReadLine();
-                        Console.ReadKey();
-                        Console.WriteLine("error");
-                        Run();
-                    }
+                //error statment
+                else
+                {
+                    Console.WriteLine("error");
+                    Console.Clear();
+                    Run();
                 }
             }
-            
-        }
+
     }
 }
-
-
-
